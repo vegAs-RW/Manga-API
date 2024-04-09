2bfc785e4969b3c87fa81a75c5b598eb9a1a28dc
vegAs-RW <jordan.pouchot2@gmail.com>
2024-04-09 15:16:02 +0200

    Add comments

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from "express";
 
+// Middleware pour log les requetes
 export const requestLogger = (req: Request, res: Response, next: NextFunction) => {
     const ninja = req.cookies.Ninja
     console.log(`${ninja} à fais une requête [${req.method}] sur la route [${req.path}]`);
--- src/middleware/randomNinja.ts
+++ src/middleware/randomNinja.ts
@@ -3,7 +3,7 @@ import { Request, Response, NextFunction } from "express";
 
 
 const ninjas = ['Naruto', 'Itachi', 'Sasuke', 'Konoha Maru', 'Sasori', 'Pain', 'Kakashi'];
-
+// Middleware pour attribuer un nom de ninja random 
 export const RandomNinjaMW = (req: Request, res: Response, next: NextFunction) => {
     if (!req.cookies.ninja) {
         const randomIndex = Math.floor(Math.random()* ninjas.length);
