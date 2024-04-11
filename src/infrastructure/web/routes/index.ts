3280e4db46404205c1d21a5618f8a91b7eb1d812
vegAs-RW <jordan.pouchot2@gmail.com>
2024-04-11 14:50:09 +0200

    Create auth routes for login & register

--- src/infrastructure/web/routes/index.ts
+++ src/infrastructure/web/routes/index.ts
@@ -1,8 +1,12 @@
 import express from "express";
 import mangaRoutes from './mangaRoutes'
+import userRoutes from './userRoutes'
+import authRoutes from './authRoutes'
 
 const router = express.Router();
 
 router.use('/mangas', mangaRoutes)
+router.use('/users', userRoutes)
+router.use('/auth', authRoutes)
 
 export default router
