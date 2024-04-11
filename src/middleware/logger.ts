550beb346d679dc9efa88cf7f664dff416f9856d
vegAs-RW <jordan.pouchot2@gmail.com>
2024-04-11 19:34:19 +0200

    Fix typo

--- src/middleware/logger.ts
+++ src/middleware/logger.ts
@@ -10,7 +10,7 @@ export const requestLogger = (req: Request, res: Response, next: NextFunction) =
     // Récupère la valeur du cookie 'Ninja' de la requête
     const ninja = req.cookies.Ninja
     // Enregistre les informations de la requête dans la console
-    console.log(`${ninja} à fais une requête [${req.method}] sur la route [${req.path}]`);
+    console.log(`${ninja} a fais une requête [${req.method}] sur la route [${req.path}]`);
     // Passe au middleware suivant ou au contrôleur suivant
     next();
 } 
