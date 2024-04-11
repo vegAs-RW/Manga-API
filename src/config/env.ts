2fc3947b4e8b006733fbfc33e588b92137a2ef99
vegAs-RW <jordan.pouchot2@gmail.com>
2024-04-11 14:49:20 +0200

    Create refreshToken mw

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,7 +2,9 @@ import { EnvConfig } from "../types/env";
 
 const env: EnvConfig = {
     PORT: parseInt(process.env.PORT || "8000"),
-    NODE_ENV: process.env.NODE_ENV as 'development' | 'production' | 'test' || 'development'
+    NODE_ENV: process.env.NODE_ENV as 'development' | 'production' | 'test' || 'development',
+    JWT_SECRET: process.env.JWT_SECRET || "MonS3cr3tTropBienGardé123:!",
+    REFRESH_TOKEN: process.env.REFRESH_TOKEN || "dxgbvxdgdsfgd"
 }
 
 export default env;
--- src/types/env.ts
+++ src/types/env.ts
@@ -1,4 +1,7 @@
 export interface EnvConfig {
     PORT: number;
     NODE_ENV: 'development' | 'production' | 'test';
+    JWT_SECRET: string;
+    REFRESH_TOKEN: string;
+
 }
