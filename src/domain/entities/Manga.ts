ae40d519f18d6fef2a734dab80a87a19e5ceed1c
vegAs-RW <jordan.pouchot2@gmail.com>
2024-04-11 15:21:20 +0200

    update Manga entitie for add author

--- src/domain/entities/Manga.ts
+++ src/domain/entities/Manga.ts
@@ -1,9 +1,11 @@
+import { MangaAuthor } from "./MangaAuthor";
+
 export interface Manga {
     id?: string;
     title: string;
-    author: string;
     genre: string[];
     status: string;
     chapters?: number;
     description: string;
+    author?: MangaAuthor[];
 }
