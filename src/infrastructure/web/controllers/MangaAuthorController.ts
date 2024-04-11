5ca12f75852b28bbd593130b435fba18f9530634
vegAs-RW <jordan.pouchot2@gmail.com>
2024-04-11 15:29:22 +0200

    Create Author controller

---
+++ src/infrastructure/web/controllers/MangaAuthorController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+import { APIResponse } from "../../../utils/APIresponse";
+import { MangaAuthorRepository } from "../../repositories/MangaAuthorRepository";
+
+const authorRepository = new MangaAuthorRepository();
+
+/**
+ * Afficher à l'écran les datas comments via une requete avec l'id d'un post
+ * @param req - requete http gérée via express
+ * @param res - réponse http gérée par express
+ */
+export const getCommentsByPostId = (req: Request, res: Response) => {
+    const {id} = req.params
+    const comments = authorRepository.getAuthorByMangaId(id)
+    APIResponse(res, {
+        statusCode: 200,
+        message: "Ok",
+        data: comments
+    })
+}
