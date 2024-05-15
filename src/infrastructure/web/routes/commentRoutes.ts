88864dcd7cad2f693774c166c9b14a2ee4b16fc9
vegAs-RW <jordan.pouchot2@gmail.com>
2024-05-15 18:48:36 +0200

    Export router for barrel export

--- src/infrastructure/web/routes/commentRoutes.ts
+++ src/infrastructure/web/routes/commentRoutes.ts
@@ -7,3 +7,6 @@ const router = express.Router();
 router.get('/:id', getCommentsByMangaId);
 router.delete('/:id', deleteCommentById)
 router.post('/:mangasId', createComment)
+
+// Exporte le routeur pour une utilisation dans d'autres fichiers
+export default router;
