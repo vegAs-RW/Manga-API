c3190b7b5cadf6dab0c1a44458cd6a0bd460842a
vegAs-RW <jordan.pouchot2@gmail.com>
2024-05-15 09:52:46 +0200

    Modify env files and types for db url

--- src/config/env.ts
+++ src/config/env.ts
@@ -4,7 +4,9 @@ const env: EnvConfig = {
     PORT: parseInt(process.env.PORT || "8000"),
     NODE_ENV: process.env.NODE_ENV as 'development' | 'production' | 'test' || 'development',
     JWT_SECRET: process.env.JWT_SECRET || "MonS3cr3tTropBienGardé123:!",
-    REFRESH_TOKEN: process.env.REFRESH_TOKEN || "dxgbvxdgdsfgd"
+    REFRESH_TOKEN: process.env.REFRESH_TOKEN || "dxgbvxdgdsfgd",
+    FRONTEND_URL: process.env.FRONTEND_URL || "http://localhost:5173",
+    DATABASE_URL: process.env.DATABASE_URL || "postgres://postgres:admin@localhost:5432/blog",
 }
 
 export default env;
--- src/types/env.ts
+++ src/types/env.ts
@@ -3,5 +3,6 @@ export interface EnvConfig {
     NODE_ENV: 'development' | 'production' | 'test';
     JWT_SECRET: string;
     REFRESH_TOKEN: string;
-
+    FRONTEND_URL: string;
+    DATABASE_URL: string;
 }
