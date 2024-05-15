3ef80189e386857985048ef1025f8b24feb7043b
vegAs-RW <jordan.pouchot2@gmail.com>
2024-05-15 18:49:17 +0200

    Update global router

--- src/infrastructure/web/routes/index.ts
+++ src/infrastructure/web/routes/index.ts
@@ -7,6 +7,11 @@ import userRoutes from './userRoutes'
 import authRoutes from './authRoutes'
 // Importe les routes pour les auteurs de manga
 import mangaAuthorRoutes from './mangaAuthorRoutes'
+// Importe les routes pour les commentaires
+import commentRoutes from './commentRoutes'
+// Importe les routes pour les categories de mangas
+import categoryRoutes from './mangaCategoryRoutes'
+
 import { isAuth } from "../../../middleware/authMiddleware";
 // Crée un routeur Express
 const router = express.Router();
@@ -19,6 +24,10 @@ router.use('/mangas',isAuth, mangaRoutes)
 router.use('/author', isAuth, mangaAuthorRoutes)
 // Utilise les routes pour les utilisateurs sur le préfixe '/users'
 router.use('/users',isAuth, userRoutes)
+// Utilise les routes pour les commentaires sur le préfixe '/comments'
+router.use('/comments', isAuth, commentRoutes)
+// Utilise les routes pour les categories sur le préfixe '/categories'
+router.use('/categories', isAuth, categoryRoutes)
 
 // Exporte le routeur pour une utilisation dans d'autres fichiers
 export default router
