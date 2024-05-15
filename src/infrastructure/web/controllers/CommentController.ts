6f110da9ab650ba818259805f40a023b74b187e9
vegAs-RW <jordan.pouchot2@gmail.com>
2024-05-15 18:28:23 +0200

    Create controller for comment services

---
+++ src/infrastructure/web/controllers/CommentController.ts
@@ -0,0 +1,42 @@
+import { Request, Response } from "express";
+import { APIResponse } from "../../../utils/APIresponse";
+import { CommentService } from "../../../domain/services/CommentService";
+
+const commentService = new CommentService();
+
+export const createComment = async (req: Request, res: Response) => {
+    try {
+        const { mangasId } = req.params;
+        const { userId } = req.user;
+        const { content } = req.body;
+        const newComment = { content, mangasId, author: userId }
+        await commentService.createComment(newComment);
+        APIResponse(res, { statusCode: 201, message: 'Comment created' });
+    } catch (err) {
+        APIResponse(res, { statusCode: 500, message: 'Internal server Error' });
+    }
+    
+}
+
+export const deleteCommentById = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const { userId } = req.user;
+        await commentService.deleteCommentById(id, userId);
+        APIResponse(res, { statusCode: 200, message: 'Comment deleted' });
+    } catch (err) {
+        APIResponse(res, { statusCode: 500, message: 'Internal server Error' });
+    }
+}
+
+export const getCommentsByMangaId = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const comments = await commentService.getCommentById(id);
+        console.table(comments)
+        APIResponse(res, { statusCode: 200, data: comments, message: 'OK' });
+    } catch (err) {
+        APIResponse(res, { statusCode: 500, message: 'Internal server Error' });
+    }
+    
+};
