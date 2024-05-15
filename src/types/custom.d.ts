786337923b2724838b85adbca753b64098ecf257
vegAs-RW <jordan.pouchot2@gmail.com>
2024-05-15 18:29:10 +0200

    update custom request

--- src/types/custom.d.ts
+++ src/types/custom.d.ts
@@ -2,7 +2,7 @@ import { Request } from "express";
 
 declare module "express-serve-static-core" {
     interface Request {
-        user?: {id: string, name: string}
+        user: {userId: string, name: string}
     }
 }
 
