1197479072ac73271d5df7345a00777590ae7a98
vegAs-RW <jordan.pouchot2@gmail.com>
2024-05-15 15:10:40 +0200

    update AuthService

--- src/domain/services/AuthService.ts
+++ src/domain/services/AuthService.ts
@@ -1,6 +1,9 @@
 import jwt from "jsonwebtoken";
 import env from "../../config/env";
 
+import { UserRepository } from "../../infrastructure/repositories/UserRepository";
+import { User } from "../entities/User";
+
 // Récupération des clés secrètes JWT depuis les variables d'environnement
 
 const { JWT_SECRET, REFRESH_TOKEN } = env
@@ -8,6 +11,7 @@ const { JWT_SECRET, REFRESH_TOKEN } = env
 export class AuthService {
     // Map pour stocker les jetons de rafraîchissement associés à l'ID de l'utilisateur
     private refreshTokenStore: Map<string, string> = new Map();
+    private UserRepository = new UserRepository()
 
     /**
      * Crée un jeton d'accès JWT avec l'ID de l'utilisateur comme payload
@@ -24,9 +28,12 @@ export class AuthService {
      * @param id Identifiant de l'utilisateur
      * @returns Jeton de rafraîchissement JWT
      */
-    createRefreshToken(id: string): string {
+    async createRefreshToken(id: string): Promise<string> {
         const refreshToken = jwt.sign({ userId: id }, REFRESH_TOKEN, { expiresIn: '5d' });
-        this.refreshTokenStore.set(id, refreshToken)
+        const user = await this.UserRepository.getUserById(id, {id: true, refreshToken: true})
+        if (user) {
+            this.UserRepository.updateUser({...user, refreshToken: refreshToken}as User)
+        }
         return refreshToken
     }
 
@@ -35,24 +42,34 @@ export class AuthService {
      * @param refreshToken Jeton de rafraîchissement JWT
      * @returns Nouveau jeton d'accès JWT ou undefined si le rafraîchissement échoue
      */
-    refreshAccessToken(refreshToken: string): string | void {
+    async refreshAccessToken(refreshToken: string): Promise<string | void> {
         try {
             // Vérifie que le jeton de rafraîchissement est valide et récupère le payload
-            const payload = jwt.decode(refreshToken) as jwt.JwtPayload;
+            const payload = jwt.verify(refreshToken, REFRESH_TOKEN) as jwt.JwtPayload;
+            const user = await this.UserRepository.getUserById(payload.userId, {id:true, refreshToken: true})
+
+            if (user && user.refreshToken === refreshToken) {
+                // On génère un nouveau token d'accès
+                return this.createAccessToken(payload.userId);
+            }
             // Récupère le jeton de rafraîchissement associé à l'ID de l'utilisateur dans le store
             const storedRefreshToken = this.refreshTokenStore.get(payload.userId)
             // Vérifie que le jeton de rafraîchissement fourni correspond à celui stocké
             if (storedRefreshToken === refreshToken) {
                 // Génère un nouveau jeton d'accès
-                const newToken = this.createAccessToken(payload.userId)
+                const newAccessToken = this.createAccessToken(payload.userId)
                 // Met à jour le jeton de rafraîchissement dans le store
-                this.refreshTokenStore.set(payload.userId, newToken)
+                return newAccessToken;
                 
             } else {
-                // Jeton de rafraîchissement invalide
-                throw new Error('Invalid refresh token')
+                if (user) {
+                    user.refreshToken = ''
+                    this.UserRepository.updateUser(user as User);
+                }
+                throw new Error('Invalid refresh token');
             }
-        } catch {
+        } catch (err) {
+            console.error(err)
             // Jeton de rafraîchissement invalide
             throw new Error('Invalid refresh token')
         }
