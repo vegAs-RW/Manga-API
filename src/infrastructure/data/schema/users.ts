2a7425b21f97d8c4031b89814d7d951596707094
vegAs-RW <jordan.pouchot2@gmail.com>
2024-05-15 16:25:58 +0200

    update user schema for refreshToken

--- src/infrastructure/data/schema/users.ts
+++ src/infrastructure/data/schema/users.ts
@@ -3,5 +3,6 @@ import { pgTable, uuid, varchar } from "drizzle-orm/pg-core";
 export const users = pgTable('users', {
     id: uuid('id').defaultRandom().primaryKey(),
     username: varchar('username', {length: 100}).notNull(),
-    password: varchar('password', {length: 255}).notNull()
+    password: varchar('password', {length: 255}).notNull(), 
+    refreshToken: varchar('refreshToken', {length: 255})
 })
