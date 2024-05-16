8c2f036af13a8e118f610078df912bad46b457d9
vegAs-RW <jordan.pouchot2@gmail.com>
2024-05-16 18:00:26 +0200

    reset migration

--- src/infrastructure/data/drizzle/0000_blue_fallen_one.sql
+++ src/infrastructure/data/drizzle/0000_blue_fallen_one.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS "authors" (
 	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
 	"fullName" varchar(255) NOT NULL,
 	"description" text NOT NULL,
-	"birthdate" timestamp NOT NULL
+	"birthdate" date NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "categories" (
@@ -24,8 +24,20 @@ CREATE TABLE IF NOT EXISTS "mangas" (
 	"title" varchar(255) NOT NULL,
 	"description" text NOT NULL,
 	"author" uuid NOT NULL,
-	"releaseDate" timestamp NOT NULL,
+	"releaseDate" date NOT NULL,
 	"category" uuid NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "messages" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"content" text NOT NULL,
+	"author" uuid NOT NULL,
+	"roomId" uuid NOT NULL,
+	"date" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE IF NOT EXISTS "rooms" (
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "users" (
@@ -55,6 +67,18 @@ END $$;
 --> statement-breakpoint
 DO $$ BEGIN
  ALTER TABLE "mangas" ADD CONSTRAINT "mangas_category_categories_id_fk" FOREIGN KEY ("category") REFERENCES "public"."categories"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "messages" ADD CONSTRAINT "messages_author_users_id_fk" FOREIGN KEY ("author") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "messages" ADD CONSTRAINT "messages_roomId_rooms_id_fk" FOREIGN KEY ("roomId") REFERENCES "public"."rooms"("id") ON DELETE no action ON UPDATE no action;
 EXCEPTION
  WHEN duplicate_object THEN null;
 END $$;
