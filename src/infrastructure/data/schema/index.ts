96f5563958017eb61b2fd3b63bf20942401e892c
vegAs-RW <jordan.pouchot2@gmail.com>
2024-05-16 18:01:12 +0200

    Update barrel export

--- src/infrastructure/data/schema/index.ts
+++ src/infrastructure/data/schema/index.ts
@@ -3,7 +3,9 @@ import { mangas } from "./mangas";
 import { categories } from "./categories";
 import { authors } from "./authors";
 import { comments } from "./comments";
+import { messages } from "./messages";
+import { rooms } from "./rooms";
 
 export {
-    users, mangas, categories, authors, comments
+    users, mangas, categories, authors, comments, messages, rooms
 }
